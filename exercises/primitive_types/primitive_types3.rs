394fd3f7684d2a6e4cfb6a441fbefede984a2fc4
G <dont_email_me_please@gmail.com>
2023-11-17 22:40:10 +0100

    Up until primites type 5

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,15 +5,13 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a: Vec<i32> = [0; 100].to_vec();
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
-        println!("Meh, I eat arrays like that for breakfast.");
+        println!("Meh, I eat arrays like that for breakfast {}.", a.len());
         panic!("Array not big enough, more elements needed")
     }
 }
